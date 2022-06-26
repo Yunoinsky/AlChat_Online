7d876b4d1f4b7c397343a98537ae74075f7f1b0c
Yunoinsky <wenchen9@qq.com>
2022-06-27 01:31:26 +0800

    :tada: Create AlChat Online

---
+++ hello_http.ts
@@ -0,0 +1,48 @@
+import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.145.0/http/file_server.ts";
+
+const servePage = async (html: string) => {
+  try {
+    const pageContents = new TextDecoder().decode(await Deno.readFile(`pages/${html}`));
+    return new Response(pageContents, {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8',
+      },
+    });
+  } catch {
+    return new Response('', {status: 400});
+  }
+}
+
+const log = (msg: string) => {
+  console.log(`${(new Date()).toISOString()} ${msg}`);
+};
+
+const handler = (req: Request) => {
+  const url = new URL(req.url);
+  if (req.method === 'GET') {
+    if (url.pathname === '/') {
+      return servePage('index.html')
+    } else if (url.pathname === '/alchat') {
+      return servePage('alchat.html')
+    } else if (url.pathname === '/data') {
+      log("Jump to 8080 port");
+    } else if (url.pathname === '/favicon.ico') {
+      return serveFile(req, 'yunofly.ico');
+    } else if (url.pathname.startsWith('/static')) {
+      const fileName = url.pathname.substring('/static/'.length);
+      return serveFile(req, 'pages/'+fileName);
+    }
+  } else if (req.method === 'POST') {
+    return new Response('', {status: 200});
+  }
+  return new Response('You are entering the void!\n', {status: 404});
+
+};
+
+const port = 80;
+log(`http://localhost:${port}/`)
+
+await serve(handler, { port });
+
---
+++ pages/index.js
@@ -0,0 +1,61 @@
+const App = {
+  data() {
+    return {
+      message: "Hello Element Plus",
+    };
+  },
+};
+
+const app = Vue.createApp(App);
+for (const [key, comp] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, comp);
+}
+app.use(ElementPlus);
+
+const YunoMenu = {
+  data() {
+    return {
+      activeIndex: "4",
+    };
+  },
+  methods: {
+    handleSelect(key, _keyPath) {
+        if (key==4) {
+            console.log(key);
+            window.location.href="./alchat";
+        }
+    },
+  },
+
+  template: `
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="1" title="集虚纳空，以至心斋">
+      <el-icon>
+        <home-filled />
+      </el-icon>
+      集虚斋
+    </el-menu-item>
+    <el-menu-item index="2" title="好好学习，天天向上">
+      <el-icon>
+        <sunrise />
+      </el-icon>
+      每日进化中！
+    </el-menu-item>
+    <el-menu-item index="3" title="来听听虫鸣吧">
+      <el-icon>
+        <headset />
+      </el-icon>
+      夏日虫语
+    </el-menu-item>
+    <el-menu-item index="4" title="基于词嵌入与社群网络的博物学文本分析">
+      <el-icon>
+        <histogram />
+      </el-icon>
+      ALChat 博物学
+    </el-menu-item>
+     </el-menu>`,
+};
+
+app.component("yuno-menu", YunoMenu);
+app.mount(".index");
+
